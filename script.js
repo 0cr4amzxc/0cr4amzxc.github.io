536413cb95198cb2bf18099a534eba1835571a77
Ocr4amzxc <jihuacuti3210@gmail.com>
2022-09-04 01:11:23 -0400

    aniadiendo todo lo que es javascritp del proyecto

---
+++ script.js
@@ -0,0 +1,31 @@
+const entradaT = document.querySelector(".entradaTexto");
+const salidaT = document.querySelector(".salidaTexto");
+const matrizCon = [["e","enter"], ["i", "imes"],["a","ai"],["o","ober"],["u","ufat"]];
+
+function btnEncriptar(){
+    const textEncriptado = encriptar(entradaT.value);
+    salidaT.value = textEncriptado;
+}
+function btnDescriptar(){
+    const textDescriptado = desencriptar(entradaT.value);
+    salidaT.value = textDescriptado;
+}
+
+function encriptar(textAencriptar){    
+    textAencriptar = textAencriptar.toLowerCase();
+    for(let i=0;i<matrizCon.length;i++){
+        if(textAencriptar.includes(matrizCon[i][0])){
+            textAencriptar = textAencriptar.replaceAll(matrizCon[i][0],matrizCon[i][1]);            
+        }
+    }
+    return textAencriptar;
+}
+function desencriptar(textDesencriptar){
+    textDesencriptar = textDesencriptar.toLowerCase();
+    for(let i=0;i<matrizCon.length;i++){
+        if(textDesencriptar.includes(matrizCon[i][1])){
+            textDesencriptar = textDesencriptar.replaceAll(matrizCon[i][1],matrizCon[i][0]);
+        }
+    }
+    return textDesencriptar;
+}
